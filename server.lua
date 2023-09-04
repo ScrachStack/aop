b9139bb974736609f64924fc7137cc926d9c1882
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-04 16:01:54 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,3 +5,7 @@ RegisterCommand("setaop", function(source, args, rawCommand)
   PATROL = newAOP
   TriggerClientEvent("aop:update", -1, PATROL)
 end, true)  
+
+AddEventHandler("playerSpawned", function()
+  TriggerClientEvent("aop:update", source, currentAOP)
+end)
