77021b0ae0d9e1171d9baa9a631103848f226303
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-04 16:22:09 -0700

    Update client.lua
    * Cleaned Up Unneeded Code
    * Fixed The Bug Before.

--- client.lua
+++ client.lua
@@ -1,20 +1,16 @@
-Citizen.CreateThread(function()
-    SetNuiFocus(false, false)
-  
-    SendNUIMessage({
-      action = "updateAOP",
-      aop = "Area Of Patrol Has Not Been Set. Use /setaop"
-    })
-  end)
-  
-  RegisterNetEvent('aop:update')
-  AddEventHandler('aop:update', function(aop)
-    SendNUIMessage({
-      action = "updateAOP",
-      aop = aop
-    })
-  end)
-  
-AddEventHandler("playerSpawned", function()
-  TriggerEvent("aop:update", currentAOP)
+RegisterNetEvent('aop:update')
+AddEventHandler('aop:update', function(aop)
+  SendNUIMessage({
+    action = "updateAOP",
+    aop = aop
+  })
 end)
+
+AddEventHandler('playerSpawned', function()
+  TriggerServerEvent('requestCurrentAOP')
+end)
+
+SendNUIMessage({
+  action = "updateAOP",
+  aop = "Area Of Patrol Has Not Been Set. Use /setaop"
+})
