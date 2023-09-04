46a6bca08e59d415e0e7a142310f7b8f4f70f995
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-04 15:24:58 -0700

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,7 @@
+local PATROL 
+
+RegisterCommand("setaop", function(source, args, rawCommand)
+  local newAOP = table.concat(args, " ")
+  PATROL = newAOP
+  TriggerClientEvent("aop:update", -1, PATROL)
+end, true)  
