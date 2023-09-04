7662564e78395179f84f69ef5c8b4f32979d9686
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-04 16:21:27 -0700

    Update server.lua
    playerspawned should deff be server sided 💀

--- server.lua
+++ server.lua
@@ -1,8 +1,12 @@
-local PATROL 
+local PATROL
 
 RegisterCommand("setaop", function(source, args, rawCommand)
   local newAOP = table.concat(args, " ")
   PATROL = newAOP
   TriggerClientEvent("aop:update", -1, PATROL)
-end, true)  
+end, true)
 
+RegisterServerEvent('request')
+AddEventHandler('request', function()
+  TriggerClientEvent("aop:update", source, PATROL)
+end)
