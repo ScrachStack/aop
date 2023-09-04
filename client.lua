5dacce0051c4f2fe8d95ce6f359ef3abdc39ec77
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-04 15:23:27 -0700

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,17 @@
+Citizen.CreateThread(function()
+    SetNuiFocus(false, false)
+  
+    SendNUIMessage({
+      action = "updateAOP",
+      aop = "Area Of Patrol Has Not Been Set. Use /setaop"
+    })
+  end)
+  
+  RegisterNetEvent('aop:update')
+  AddEventHandler('aop:update', function(aop)
+    SendNUIMessage({
+      action = "updateAOP",
+      aop = aop
+    })
+  end)
+  
